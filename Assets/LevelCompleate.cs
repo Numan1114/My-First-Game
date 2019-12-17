3d50795abf25ba574a4cad4ee6820e57a6a41c0e
Numan1114 <Numan1114@users.noreply.github.com>
2019-12-16 19:02:51 -0500

    Initial commit

---
+++ Assets/LevelCompleate.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelCompleate : MonoBehaviour
+{
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
